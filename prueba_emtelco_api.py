5c386491c63b0ddffa04b04860b65b4790ea7c62
Alexander Marin <Alexandermarinvilla@gmail.com>
2025-03-26 23:14:54 -0500

    Prueba2

--- prueba_emtelco_api.py
+++ prueba_emtelco_api.py
@@ -18,26 +18,14 @@ def obtener_vulnerabilidades():
         print("Error")
 
 def obtener_vulnerabilidades_fixeadas():
-    """Obtiene y muestra las vulnerabilidades marcadas como fixeadas."""
-    # TODO: Implementar la lógica para obtener vulnerabilidades fixeadas.
-    print("Función para obtener vulnerabilidades fixeadas (no implementada).")
+    print("Prueba punto 2.")
 
 def obtener_vulnerabilidades_no_fixeadas():
-    """Obtiene y muestra las vulnerabilidades no fixeadas."""
-    # TODO: Implementar la lógica para obtener vulnerabilidades no fixeadas.
-    print("Función para obtener vulnerabilidades no fixeadas (no implementada).")
+    print("Prueba punto 3.")
 
 def obtener_informacion_severidad():
-    try:
-        if "vulnerabilities" in datos:
-            for indice, cve in enumerate(datos["vulnerabilities"], 1): #agrega indice e imprime desde el 1 en adelante las vulnerabilidades encontradas
-                print(f"{indice}. {cve['metrics']['cvssMetricV2']['baseSeverity']}")
-        else:
-            print("No se encontraron vulnerabilidades.")
-
-    except requests.exceptions.RequestException as e:
-        print("Error")
-
+    print("Prueba punto 3.")
+    
 def mostrar_menu():
     """Muestra el menú de opciones."""
     print("Por favor, elija una de las siguientes opciones:")
@@ -69,8 +57,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
